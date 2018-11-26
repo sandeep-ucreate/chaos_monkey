fd128c31c4b5cb57c89ac5fc678eb898f98136bd
Sandeep Kaur <sandeepkaur@ucreate.co.in>
2018-11-26 10:40:34 +0530

    Changes in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111 = [
+    protected $fillable = [
         'name', 'email', 'password',
     ];
 
